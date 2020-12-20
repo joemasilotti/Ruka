0c3539e87a41ba41d75d70c65de1aff924b322d6
Dominik Hauser <dom@dasdom.de>
2020-12-20 10:37:49 +0100

    Adjust animation durations

--- Sources/Ruka/Animation.swift
+++ Sources/Ruka/Animation.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct Animation {
     enum Length: TimeInterval {
-        case short = 0.01
-        case dismissAlert = 0.1
-        case popController = 0.36
+        case short = 0.05
+        case dismissAlert = 0.25
+        case popController = 0.4
     }
 }
